9c5e79334ae9f20ae743e2f43562ba7382ddf145
alen-B <alenworkworld@gmail.com>
2021-05-20 11:00:16 +0800

    Catch sending information exception

--- ninja/app/src/main/java/com/ninjahome/ninja/viewmodel/ChatViewModel.kt
+++ ninja/app/src/main/java/com/ninjahome/ninja/viewmodel/ChatViewModel.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.text.TextUtils
 import androidlib.Androidlib
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.rxLifeScope
 import com.ninja.android.lib.base.BaseViewModel
 import com.ninja.android.lib.command.BindingAction
 import com.ninja.android.lib.command.BindingCommand
@@ -15,13 +16,18 @@ import com.ninjahome.ninja.model.bean.TextData
 import com.ninjahome.ninja.ui.activity.contact.detail.ContactDetailActivity
 import com.ninjahome.ninja.utils.ChatMessageFactory
 import com.ninjahome.ninja.utils.JsonUtils
+import com.orhanobut.logger.Logger
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import org.greenrobot.eventbus.EventBus
+import org.koin.core.component.KoinApiExtension
 
 /**
  *Author:Mr'x
  *Time:
  *Description:
  */
+@KoinApiExtension
 class ChatViewModel : BaseViewModel() {
     lateinit var uid: String
     var message = MutableLiveData<String>()
@@ -38,10 +44,17 @@ class ChatViewModel : BaseViewModel() {
                 showToast(R.string.input_send_message)
                 return
             }
-            Androidlib.writeMessage(uid, JsonUtils.object2Json(TextData(message.value!!, 0), TextData::class.java).toByteArray())
-            val chatMessage = ChatMessageFactory.createChatMessage(NinjaApp.instance.account.address, uid, "https://img0.baidu.com/it/u=232786431,3173227563&fm=26&fmt=auto&gp=0.jpg", true, message.value!!)
+            rxLifeScope.launch ({
+                withContext(Dispatchers.IO){
+                    Androidlib.writeMessage(uid, JsonUtils.object2Json(TextData(message.value!!, 0), TextData::class.java).toByteArray())
+                    val chatMessage = ChatMessageFactory.createChatMessage(NinjaApp.instance.account.address, uid, "https://img0.baidu.com/it/u=232786431,3173227563&fm=26&fmt=auto&gp=0.jpg", true, message.value!!)
+                    EventBus.getDefault().postSticky(EventSendConversation(uid, NinjaApp.instance.account.address, chatMessage))
 
-            EventBus.getDefault().postSticky(EventSendConversation(uid, NinjaApp.instance.account.address, chatMessage))
+                }
+            },{
+                showToast(R.string.send_error)
+                Logger.e("send error ${it.message}")
+            })
 
 
         }
